b0d45266c57a7979075036a4fb34b941230a7ab1
Szymon Rakowski <duch_rak@interia.pl>
2020-02-09 17:27:55 +0100

    s

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,16 +1,10 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
-
-// You can delete this file if you're not using it
-
 const React = require("react")
 const Layout = ...
 const MobileLayout = ...
 
 exports.wrapPageElement = ({ element, props }) => {
+
+   // a made up hook to detect browser size, implement your own
    const isMobile = useDetectMobile()
 
   return (
