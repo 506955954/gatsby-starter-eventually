4e6abf0be76f19a721cc9177509e1dd78ae3b859
Szymon Rakowski <duch_rak@interia.pl>
2020-02-09 15:49:32 +0100

    Update config.js

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ module.exports = {
   // social
   heading: 'Filmmaker ░ Photographer ░ Nomad',
   subHeading:
-    "Follow my works by the ICONS below",
+    "Follow my works through the ICONS below",
   socialLinks: [
     {
       icon: 'fa-youtube',
