127d576d0ee8e3132975031d31552f8eee2dedd0
Szymon Rakowski <duch_rak@interia.pl>
2020-02-10 02:17:01 +0100

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -38,7 +38,7 @@ class Layout extends Component {
             <Helmet
               title={data.site.siteMetadata.title}
               meta={[
-                { name: 'description', content: 'Szymon Rakowski' },
+                { name: 'description', content: 'Cinematographer' },
                 { name: 'keywords', content: 'site, web' },
               ]}
             >
