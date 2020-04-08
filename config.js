347743ee482bbd746ca0303d752b03ee9275b98a
Szymon Rakowski <duch_rak@interia.pl>
2020-04-09 01:57:40 +0200

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   manifestIcon: 'src/assets/img/initials.png',
   pathPrefix: `/gatsby-starter-eventually/`, // This path is subpath of your hosting https://domain/portfolio
   // social
-  heading: 'Cinematographer ░ Nomad',
+  heading: 'Cinematographer ® Wanderer',
   subHeading:
     "Follow my works through the ICONS below",
   socialLinks: [
