8c4e6ad4f42a3cbba07ccaddb08a635a8b324200
Szymon Rakowski <duch_rak@interia.pl>
2020-05-11 01:44:39 +0200

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -17,7 +17,7 @@ export default function Footer() {
         })}
       </ul>
       <ul className="copyright">
-        <li> Szymon Rakowski</li>
+        <li> szymonrakowski@protonmail.com</li>
         <li>
           Currently in Suwałki, Poland
         </li>
