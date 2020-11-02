0dd2e2cc87a96221a9f7e80c90cbe64da031fcba
Szymon Rakowski <duch_rak@interia.pl>
2020-11-02 15:53:04 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,7 @@ module.exports = {
   manifestIcon: 'src/assets/img/initials.png',
   pathPrefix: `/gatsby-starter-eventually/`, // This path is subpath of your hosting https://domain/portfolio
   // social
-  heading: 'Cinematographer ® Operator Filmowy',
+  heading: '🇬🇧Cinematographer ® Operator Filmowy',
   subHeading:
     "Follow my works through the ICONS below ↓",
   socialLinks: [
