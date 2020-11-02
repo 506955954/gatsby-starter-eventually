ca8accfbbd1f46008a47a6b7a875726a54466157
Szymon Rakowski <duch_rak@interia.pl>
2020-11-02 16:15:14 +0100

    Update config.js

--- config.js
+++ config.js
@@ -11,8 +11,9 @@ module.exports = {
   // social
   heading: 'Cinematographer ® 🇵🇱 Operator Filmowy',
   subHeading:
-    "Follow my works through the ICONS below ↓
-     Moje prace - kliknij IKONĘ ↓",
+    "Follow my works through the ICONS below ↓",
+  subHeading:
+    "Moje prace - kliknij IKONĘ ↓",
   socialLinks: [
     {
       icon: 'fa-youtube',
