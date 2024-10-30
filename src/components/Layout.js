6ea72c2f742b4b853a1a3bfc81b1003189aa8cb7
506955954 <60741051+506955954@users.noreply.github.com>
2024-10-30 13:01:22 +0100

    Update Layout.js

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,61 +1,36 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import Helmet from 'react-helmet';
-import { StaticQuery, graphql } from 'gatsby';
+import React from 'react'
+import PropTypes from 'prop-types'
+import Helmet from 'react-helmet'
+import { StaticQuery, graphql } from "gatsby"
 
-import '../assets/sass/main.scss';
-
-class Layout extends Component {
-  constructor() {
-    super();
-    this.state = {
-      isPreloaded: true,
-    };
-  }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({ isPreloaded: false });
-    }, 100);
-  }
-
-  render() {
-    const { children } = this.props;
-    const { isPreloaded } = this.state;
-    return (
-      <StaticQuery
-        query={graphql`
-          query SiteTitleQuery {
-            site {
-              siteMetadata {
-                title
-              }
-            }
+const Layout = ({ children, data }) => (
+  <StaticQuery
+    query={graphql`
+      query SiteTitleQuery {
+        site {
+          siteMetadata {
+            title
           }
-        `}
-        render={data => (
-          <>
-            <Helmet
-              title={data.site.siteMetadata.title}
-              meta={[
-                { name: 'description', content: 'Cinematographer' },
-                { name: 'keywords', content: 'site, web' },
-              ]}
-            >
-              <html lang="en" />
-            </Helmet>
-            <div className={isPreloaded ? 'main-body is-preload' : 'main-body'}>
-              {children}
-            </div>
-          </>
-        )}
-      />
-    );
-  }
-}
+        }
+      }
+    `}
+    render={data => (
+      <React.Fragment>
+        <Helmet
+          title={data.site.siteMetadata.title}
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' },
+          ]}
+        />
+        {children}
+      </React.Fragment>
+    )}
+  />
+)
 
 Layout.propTypes = {
   children: PropTypes.node.isRequired,
-};
+}
 
-export default Layout;
+export default Layout
