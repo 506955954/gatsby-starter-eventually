a9fa9436f330b71b51e29d14460d3718fef3b25a
Szymon Rakowski <duch_rak@interia.pl>
2024-11-01 17:45:58 +0100

    f

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ import SlideShow from '../components/SlideShow';
 
 var settings = {
   images: [
-    { url: require('../assets/images/bg01.jpg'), position: 'center' },
+    { url: require('../assets/images/bg01.png'), position: 'center' },
     { url: require('../assets/images/bg02.jpg'), position: 'center' },
     { url: require('../assets/images/bg03.jpg'), position: 'center' },
   ],
